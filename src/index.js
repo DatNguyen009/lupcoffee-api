94b549d1a1c4f678af82474c887a76b59b7ee604
datnguyenth <datnguyen.gts99@gmail.com>
2023-07-25 20:08:42 +0700

    add port variable in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const indexRouter = require("./routers/indexRouter");
-
+const PORT = process.env.PORT || 3030;
 app.use(express.json());
 
 app.use("/api", indexRouter);
 
-app.listen(5000, () => {
-  console.log("server started on port 5000");
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
